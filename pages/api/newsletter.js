591d8b0a3d90d51138df1a7c80858044625c4051
Josh Worden <2015JTW@gmail.com>
2023-04-20 18:46:17 -0400

    feat: connected mongodb database

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
 
     if(req.method === 'POST'){
         const userEmail = req.body.email;
@@ -8,7 +10,11 @@ function handler(req, res) {
             return;
         }
 
-        console.log(userEmail)
+        const client = await MongoClient.connect('mongodb+srv://2015jtw:jhaCxiAYfdOIfaCF@cluster0.76gkoe5.mongodb.net/?retryWrites=true&w=majority')
+        const db = client.db();
+        await db.collection('emails').insertOne({email: userEmail})
+        client.close();
+        
         res.status(201).json({message: 'Valid Email'})
     }
 
