34911650e7f01e198fde61a7ce079dbb8cd964de
Josh Worden <2015JTW@gmail.com>
2023-05-01 16:14:00 -0400

    started implementing context api to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import '../styles/globals.css'
 import Layout from '../components/layout/layout'
+import Notification from '../components/UI/notification'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Notification
+        title="Test"
+        message="This is a test"
+        status="success"
+      />
     </Layout>
   )
 }
